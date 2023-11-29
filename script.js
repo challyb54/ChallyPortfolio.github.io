0e29fbe625867c90bcb31af2039cfb1758b217d9
Challyb54 <Challyb54@gmail.com>
2023-11-30 00:00:54 +0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -48,9 +48,9 @@ ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
 ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
  
 const typed = new Typed ('.multiple-text',{
-    strings:['Frontend Developer.','Web Designer.','Graphic Designer /', 'Digital marketer.'],
+    strings:['Software Developer.'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:2000,
-    loop:true
+    loop:false
 });
